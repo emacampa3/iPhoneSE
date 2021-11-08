db3f11abef4de74825de080451b9dad3da34ec13
Ema Čampa <85159765+emacampa3@users.noreply.github.com>
2021-11-08 21:02:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -96,25 +96,3 @@ const render = () => {
 	});
 };
 render();
-
-
-/* dark & light theme */
-/*
-const checkbox = document.querySelector('input[name=theme]');
-
-checkbox.addEventListener('change', function() {
-	if(this.checked) {
-			trans()
-			document.documentElement.setAttribute('data-theme', 'dark')
-	} else {
-			trans()
-			document.documentElement.setAttribute('data-theme', 'light')
-	}
-})
-
-let trans = () => {
-	document.documentElement.classList.add('transition');
-	window.setTimeout(() => {
-			document.documentElement.classList.remove('transition')
-	}, 10)
-}*/
